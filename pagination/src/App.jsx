b0e273f58242e1306b7374484a1ed5934a228a8a
AKASH <88474098+AKASH-GO@users.noreply.github.com>
2024-01-05 18:29:12 +0530

    pagination

--- pagination/src/App.jsx
+++ pagination/src/App.jsx
@@ -2,11 +2,14 @@ import "./App.css";
 import Pagination from "./Pagination";
 
 function App() {
+ 
+
   return (
-    <div className="h-screen">
-      <Pagination />
+    <div className='h-screen'>
+      <Pagination/>
     </div>
-  );
+  )
 }
+
 
 export default App;
